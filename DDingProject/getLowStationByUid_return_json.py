bb96739e666cc7bb28e9adf1ca86aee5571e2316
haiseong <wjdgotjd9908@gmail.com>
2021-08-27 13:06:46 +0900

    Revert "com"
    This reverts commit 86f32b2748c16184dc547075fdb100fda578cbe2.

--- DDingProject/getLowStationByUid_return_json.py
+++ DDingProject/getLowStationByUid_return_json.py
@@ -32,12 +32,9 @@ def getLowStationByUid_return_json(arsId):
     busRouteJson = []
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 =======
 =======
 >>>>>>> parent of 6aa3792 (com)
-=======
->>>>>>> parent of 6aa3792b (com)
     msgBody = tree_getLowStationByUid.find("msgBody")
     itemList = msgBody.findall("itemList")
 
@@ -63,7 +60,6 @@ def getLowStationByUid_return_json(arsId):
     for i in itemList:
         temp_dict = {}
         temp_dict['busRouteId'] = i.find("busRouteId").text
-<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         temp_dict['busRouteNm'] = i.find("busRouteNm").text
@@ -90,8 +86,6 @@ def getLowStationByUid_return_json(arsId):
 >>>>>>> parent of 6aa3792 (com)
 =======
 >>>>>>> parent of 6aa3792 (com)
-=======
->>>>>>> parent of 6aa3792b (com)
         temp_dict['rtNm'] = i.find("rtNm").text
 
         busRouteList.append(temp_dict)
